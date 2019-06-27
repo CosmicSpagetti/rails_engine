8cd0a0ef26be77f476ae0afd53d2284f86b6e292
billy urrutia <billyurrutia@gmail.com>
2019-06-27 15:21:02 -0600

    added all search controllers routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,37 +2,43 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+
+      namespace :customers do 
+        get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
+      end
+
+      namespace :invoices do 
+        get '/find', to: "search#show"
+        get '/find_all', to: 'search#index'
+      end
+
+      namespace :invoice_items do 
+        get '/find', to: "search#show"
+        get '/find_all', to: 'search#index'
+      end
+
+      namespace :items do 
+        get '/find', to: "search#show"
+        get '/find_all', to: 'search#index'
+      end
+
+      namespace :merchants do 
+        get '/find', to: "search#show"
+        get '/find_all', to: 'search#index'
+      end
+
+      namespace :transactions do 
+        get '/find', to: "search#show"
+        get '/find_all', to: 'search#index'
+      end
+
       resources :customers, only: [:index, :show]
-    end
-  end
-
-  namespace :api do 
-    namespace :v1 do 
       resources :invoices, only: [:index, :show]
-    end
-  end
-  
-  namespace :api do 
-    namespace :v1 do 
       resources :invoice_items, only: [:index, :show]
-    end 
-  end
-
-  namespace :api do 
-    namespace :v1 do 
       resources :items, only: [:index, :show]
-    end
-  end
-
-  namespace :api do 
-    namespace :v1 do 
-      resources :merchants, only: [:index, :show]
-    end 
-  end
-
-  namespace :api do 
-    namespace :v1 do 
+      resources :merchants, only: [:index, :show]  
       resources :transactions, only: [:index, :show]
-    end 
+    end
   end
 end
