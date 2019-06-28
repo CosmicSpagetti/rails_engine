ae4e00d25d48e9c27bf47a22358d9eb43552ed80
billy urrutia <billyurrutia@gmail.com>
2019-06-28 11:22:02 -0600

    setting up routes for displaying relationships

--- config/routes.rb
+++ config/routes.rb
@@ -34,10 +34,19 @@ Rails.application.routes.draw do
       end
 
       resources :customers, only: [:index, :show]
-      resources :invoices, only: [:index, :show]
+
+      resources :invoices, only: [:index, :show], module: :invoices do 
+        resources :transactions, only: :index
+      end
+
       resources :invoice_items, only: [:index, :show]
       resources :items, only: [:index, :show]
-      resources :merchants, only: [:index, :show]  
+
+      resources :merchants, only: [:index, :show], module: :merchants do 
+        resources :items, only: [:index]
+        resources :invoices, only: [:index]
+      end  
+
       resources :transactions, only: [:index, :show]
     end
   end
