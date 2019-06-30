43e25bb7970f9482637a67237b6c4dc614a09260
billy urrutia <billyurrutia@gmail.com>
2019-06-29 18:20:01 -0600

    test for invoice_item's invoices

--- spec/requests/api/v1/invoice_item/invoice_item_relationships_spec.rb
+++ spec/requests/api/v1/invoice_item/invoice_item_relationships_spec.rb
@@ -12,4 +12,16 @@ describe "Invoice_items relationships" do
     expect(response).to be_successful
     expect(invoice_item_item["id"]).to eq(item.id.to_s)
   end
+
+  it "should show invoice from specified invoice_item id" do 
+      invoice = create(:invoice)
+      invoice_item = create(:invoice_item, invoice: invoice)
+  
+      get "/api/v1/invoice_items/#{invoice_item.id}/invoice"
+  
+      invoice_item_invoice = JSON.parse(response.body)["data"]
+  
+      expect(response).to be_successful
+      expect(invoice_item_invoice["id"]).to eq(invoice.id.to_s)
+  end
 end
