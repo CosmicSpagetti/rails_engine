8daf0ef0db2bb956fe0ca1ff7da877bb3c91ab99
billy urrutia <billyurrutia@gmail.com>
2019-06-30 15:00:27 -0600

    Fixed ordering for the spec harness

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,10 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
 
+  scope :order_by_id, -> { order(id: 'asc')}
   validates_presence_of :name, :description, :unit_price 
 
   def self.find_all_by_parameters(params)
-    where(params).order(:id)
+    where(params).order(id: :asc)
   end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,7 +2,7 @@ class Transaction < ApplicationRecord
   belongs_to :invoice
 
   validates_presence_of :credit_card_number, :result
-
+  scope :order_by_id, -> { order(id: 'asc')}
   scope :successful, -> { where(result: 'success') }
 
   def self.find_all_by_parameters(params)
